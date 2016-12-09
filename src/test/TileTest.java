578456fd1cf008a41a0d86e5c49f7065c984faf5
Jose Toledo <joset2.91@gmail.com>
2016-12-09 23:13:15 +0100

    Added junit test class for Tile

---
+++ src/test/TileTest.java
@@ -0,0 +1,89 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TileTest {
+
+    @Test
+    public void testSetLineTypes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testTileLineTypeArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testTile() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetLineTypeAtIndex() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNumberOfColors() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsExactlyEqualTo() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCopy() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRotateClockwise() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRotateCounterClockwise() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsEmpty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsRotationEqualTo() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCanBeRecoloredTo() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDominates() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testHasSameColorsAs() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFitsTo() {
+        fail("Not yet implemented");
+    }
+
+}
