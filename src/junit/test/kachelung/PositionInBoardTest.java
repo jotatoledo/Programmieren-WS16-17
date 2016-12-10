df02b7d5899ff1df73b29cf17db1924295d28b89
Jose Toledo <joset2.91@gmail.com>
2016-12-10 14:49:43 +0100

    Added test class for the position in board enum

---
+++ src/junit/test/kachelung/PositionInBoardTest.java
@@ -0,0 +1,24 @@
+package junit.test.kachelung;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PositionInBoardTest {
+
+    @Test
+    public void testPositionInBoard() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPositions() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCalculatePosition() {
+        fail("Not yet implemented");
+    }
+
+}
