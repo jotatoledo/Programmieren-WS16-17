b9d901cf1f753ca88faf4e9d18a1724b4e4f7f85
Jose Toledo <joset2.91@gmail.com>
2016-12-10 01:42:55 +0100

    Added jUnit class for Board

---
+++ src/junit/test/kachelung/BoardTest.java
@@ -0,0 +1,68 @@
+package junit.test.kachelung;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.kit.informatik.kachelung.Board;
+
+public class BoardTest {
+
+    @Test
+    public void testBoard() {
+        Board firstTest = new Board();
+        assertTrue(firstTest.isEmpty() == true);
+        System.out.println(firstTest);
+    }
+
+    @Test
+    public void testGetTile() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetTile() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRemoveTile() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsEmpty() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRotateTileClockwise() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRotateTileCounterClockwise() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNumberOfColors() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsValid() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetConnectedPathColor() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+}
