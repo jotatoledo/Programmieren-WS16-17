95f28969289c33a9c3c651266bb05d6a2a8f2fa3
Jose Toledo <joset2.91@gmail.com>
2016-12-10 16:18:27 +0100

    Added encapsulation functions

--- src/edu/kit/informatik/kachelung/BoardTile.java
+++ src/edu/kit/informatik/kachelung/BoardTile.java
@@ -4,8 +4,40 @@ public class BoardTile {
     private Tile tile;
     private PositionInBoard position;
     
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+
+    public PositionInBoard getPosition() {
+        return position;
+    }
+
+    public void setPosition(PositionInBoard position) {
+        this.position = position;
+    }
+    
     public BoardTile(Tile tile, PositionInBoard position) {
         this.tile = tile;
         this.position = position;
+    }
+    
+    public boolean isEmpty() {
+        return tile.isEmpty();
+    }
+    
+    public void rotateClockwise() {
+        tile.rotateClockwise();
+    }
+    
+    public void rotateCounterClockwise() {
+        tile.rotateCounterClockwise();
+    }
+    
+    public int getNumberOfColors() {
+        return tile.getNumberOfColors();
     }
 }
