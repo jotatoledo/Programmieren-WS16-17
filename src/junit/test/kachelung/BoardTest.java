9428af7849dd41cb5bd7558a5200cec64e6b2652
Jose Toledo <joset2.91@gmail.com>
2016-12-10 11:55:06 +0100

    Added methods toString,isEmpty, setTile and empty constructor

--- src/junit/test/kachelung/BoardTest.java
+++ src/junit/test/kachelung/BoardTest.java
@@ -3,16 +3,26 @@ package junit.test.kachelung;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 import edu.kit.informatik.kachelung.Board;
+import edu.kit.informatik.kachelung.LineType;
+import edu.kit.informatik.kachelung.Tile;
 
 public class BoardTest {
-
+    
     @Test
     public void testBoard() {
         Board firstTest = new Board();
+        StringBuilder builder = new StringBuilder();
+        
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;");
+        assertTrue(firstTest != null);
         assertTrue(firstTest.isEmpty() == true);
-        System.out.println(firstTest);
+        assertThat(firstTest.toString(), is(builder.toString()));
     }
 
     @Test
@@ -22,7 +32,35 @@ public class BoardTest {
 
     @Test
     public void testSetTile() {
-        fail("Not yet implemented");
+        Board test = new Board();
+        StringBuilder builder = new StringBuilder();
+        String actualResult;
+        String expectedResult;
+        
+        test.setTile(1, new Tile(new LineType[] {
+                LineType.GREEN, LineType.GREEN, LineType.YELLOW, 
+                LineType.NONE, LineType.YELLOW, LineType.NONE }));
+        
+        builder.append("------;GGY-Y-;------;").append("\n");
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;");
+        actualResult = test.toString();
+        expectedResult = builder.toString();
+        assertThat(actualResult, is(expectedResult));
+        
+        test.setTile(4, new Tile(new LineType[] {
+                LineType.RED, LineType.GREEN, LineType.RED, 
+                LineType.GREEN, LineType.YELLOW, LineType.YELLOW }));
+    
+        builder.setLength(0);
+        builder.append("------;GGY-Y-;------;").append("\n");
+        builder.append("------;RGRGYY;------;").append("\n");
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;");        
+        actualResult = test.toString();
+        expectedResult = builder.toString();        
+        assertThat(actualResult, is(expectedResult));
     }
 
     @Test
@@ -32,7 +70,14 @@ public class BoardTest {
 
     @Test
     public void testIsEmpty() {
-        fail("Not yet implemented");
+        Board test = new Board();
+        assertTrue(test.isEmpty() == true);
+        
+        test.setTile(1, new Tile(new LineType[] {
+                LineType.GREEN, LineType.GREEN, LineType.YELLOW, 
+                LineType.NONE, LineType.YELLOW, LineType.NONE }));
+        
+        assertTrue(test.isEmpty() == false);
     }
 
     @Test
@@ -62,7 +107,21 @@ public class BoardTest {
 
     @Test
     public void testToString() {
+        Board firstTest = new Board();
+        StringBuilder builder = new StringBuilder();
+        
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;").append("\n");
+        builder.append("------;------;------;");
+        String actualResult = firstTest.toString();
+        String expectedResult = builder.toString();
+        
+        assertThat(actualResult, is(expectedResult));
+    }
+    
+    @Test
+    public void testCopy() {
         fail("Not yet implemented");
     }
-
 }
