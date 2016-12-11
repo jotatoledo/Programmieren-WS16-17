c471e56d8e63e9badca91968021886fd401b8b83
Jose Toledo <joset2.91@gmail.com>
2016-12-11 19:17:04 +0100

    Added internal documentation

--- src/edu/kit/informatik/kachelung/BoardTile.java
+++ src/edu/kit/informatik/kachelung/BoardTile.java
@@ -1,42 +1,81 @@
 package edu.kit.informatik.kachelung;
 
 public class BoardTile {
+    /**
+     * The representation of a tile in a board represented by an instance of {@linkplain board}
+     */
     private Tile tile;
+    /**
+     * The position of the tile in a board represented by an instance of {@linkplain board}
+     */
     private PositionInBoard position;
-    
-    public Tile getTile() {
-        return tile;
-    }
-
-    public void setTile(Tile tile) {
-        this.tile = tile;
-    }
-
-    public PositionInBoard getPosition() {
-        return position;
-    }
-
-    public void setPosition(PositionInBoard position) {
-        this.position = position;
-    }
     
     public BoardTile(Tile tile, PositionInBoard position) {
         this.tile = tile;
         this.position = position;
     }
     
+    /**
+     * Getter for {@link #tile}
+     * @return
+     */
+    public Tile getTile() {
+        return tile;
+    }
+
+    /**
+     * Setter for {@link tile}
+     * @param tile
+     */
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+
+    /**
+     * Getter for {@link #tile}
+     * @return
+     */
+    public PositionInBoard getPosition() {
+        return position;
+    }
+
+    /**
+     * Setter for {@link position}
+     * @param position
+     */
+    public void setPosition(PositionInBoard position) {
+        this.position = position;
+    }
+    
+    /**
+     * Checks if the {@linkplain Tile tile} has no colors.
+     * A tile has no colors when the codification list of the {@linkplain Tile} component
+     * contains only references to {@linkplain LineType #NONE NONE}
+     * @return {@code True} if the {@linkplain Tile} component has only references to {@linkplain LineType #NONE NONE}. 
+     * {@code False} otherwise.
+     */
     public boolean isEmpty() {
         return tile.isEmpty();
     }
     
+    /**
+     * Rotates the {@linkplain Tile tile} clockwise
+     */
     public void rotateClockwise() {
         tile.rotateClockwise();
     }
     
+    /**
+     * Rotates the {@linkplain Tile tile} counter clockwise
+     */
     public void rotateCounterClockwise() {
         tile.rotateCounterClockwise();
     }
     
+    /**
+     * Gets the number of colors of the {@linkplain Tile} component of the instance
+     * @return 
+     */
     public int getNumberOfColors() {
         return tile.getNumberOfColors();
     }
