00310ae4a6f420fc4c07b77ebe566cc9b1829cd1
Jose Toledo <joset2.91@gmail.com>
2016-12-12 14:05:20 +0100

    Changed test base cases to class static values

--- src/junit/test/kachelung/BoardTest.java
+++ src/junit/test/kachelung/BoardTest.java
@@ -10,64 +10,64 @@ import edu.kit.informatik.kachelung.LineType;
 import edu.kit.informatik.kachelung.Tile;
 
 public class BoardTest {
-    private final Board emptyBoard = new Board(); //Empty board
-    private final Board validBoard = new Board(); //Example in picture 9
-    private final Board invalidBoard = new Board(); //Example in picture 10
+    private static final Board EMPTY_BOARD = new Board(); //Empty board
+    private static final Board VALID_BOARD = new Board(); //Example in picture 9
+    private static final Board INVALID_BOARD = new Board(); //Example in picture 10
     
     public BoardTest() {
-        validBoard.setTile(1, new Tile(new LineType[] {
+        VALID_BOARD.setTile(1, new Tile(new LineType[] {
                 LineType.GREEN, LineType.GREEN, LineType.YELLOW, 
                 LineType.NONE, LineType.YELLOW, LineType.NONE }));
-        validBoard.setTile(2, new Tile(new LineType[] {
+        VALID_BOARD.setTile(2, new Tile(new LineType[] {
                 LineType.NONE, LineType.NONE, LineType.NONE, 
                 LineType.NONE, LineType.RED, LineType.RED }));
-        validBoard.setTile(4, new Tile(new LineType[] {
+        VALID_BOARD.setTile(4, new Tile(new LineType[] {
                 LineType.RED, LineType.GREEN, LineType.RED, 
                 LineType.GREEN, LineType.YELLOW, LineType.YELLOW }));
-        validBoard.setTile(5, new Tile(new LineType[] {
+        VALID_BOARD.setTile(5, new Tile(new LineType[] {
                 LineType.GREEN, LineType.GREEN, LineType.NONE, 
                 LineType.NONE, LineType.NONE, LineType.NONE }));
-        validBoard.setTile(6, new Tile(new LineType[] {
+        VALID_BOARD.setTile(6, new Tile(new LineType[] {
                 LineType.NONE, LineType.NONE, LineType.YELLOW, 
                 LineType.GREEN, LineType.GREEN, LineType.YELLOW }));
-        validBoard.setTile(7, new Tile(new LineType[] {
+        VALID_BOARD.setTile(7, new Tile(new LineType[] {
                 LineType.GREEN, LineType.NONE, LineType.NONE, 
                 LineType.RED, LineType.GREEN, LineType.RED }));
-        validBoard.setTile(8, new Tile(new LineType[] {
+        VALID_BOARD.setTile(8, new Tile(new LineType[] {
                 LineType.NONE, LineType.YELLOW, LineType.YELLOW, 
                 LineType.NONE, LineType.NONE, LineType.NONE }));
-        validBoard.setTile(10, new Tile(new LineType[] {
+        VALID_BOARD.setTile(10, new Tile(new LineType[] {
                 LineType.NONE, LineType.NONE, LineType.NONE, 
                 LineType.YELLOW, LineType.YELLOW, LineType.NONE }));
-        validBoard.setTile(11, new Tile(new LineType[] {
+        VALID_BOARD.setTile(11, new Tile(new LineType[] {
                 LineType.YELLOW, LineType.NONE, LineType.NONE, 
                 LineType.NONE, LineType.NONE, LineType.YELLOW }));
         
-        invalidBoard.setTile(1, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(1, new Tile(new LineType[] {
                 LineType.NONE, LineType.GREEN, LineType.GREEN, 
                 LineType.YELLOW, LineType.NONE, LineType.YELLOW }));        
-        invalidBoard.setTile(2, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(2, new Tile(new LineType[] {
                 LineType.RED, LineType.RED, LineType.NONE, 
                 LineType.NONE, LineType.NONE, LineType.NONE }));
-        invalidBoard.setTile(4, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(4, new Tile(new LineType[] {
                 LineType.RED, LineType.GREEN, LineType.RED, 
                 LineType.GREEN, LineType.YELLOW, LineType.YELLOW }));
-        invalidBoard.setTile(5, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(5, new Tile(new LineType[] {
                 LineType.GREEN, LineType.GREEN, LineType.NONE, 
                 LineType.NONE, LineType.NONE, LineType.NONE }));
-        invalidBoard.setTile(6, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(6, new Tile(new LineType[] {
                 LineType.YELLOW, LineType.NONE, LineType.NONE, 
                 LineType.YELLOW, LineType.GREEN, LineType.GREEN }));
-        invalidBoard.setTile(7, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(7, new Tile(new LineType[] {
                 LineType.GREEN, LineType.NONE, LineType.NONE, 
                 LineType.RED, LineType.GREEN, LineType.RED }));
-        invalidBoard.setTile(8, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(8, new Tile(new LineType[] {
                 LineType.NONE, LineType.YELLOW, LineType.YELLOW, 
                 LineType.NONE, LineType.NONE, LineType.NONE }));
-        invalidBoard.setTile(10, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(10, new Tile(new LineType[] {
                 LineType.NONE, LineType.NONE, LineType.NONE, 
                 LineType.YELLOW, LineType.YELLOW, LineType.NONE }));
-        invalidBoard.setTile(11, new Tile(new LineType[] {
+        INVALID_BOARD.setTile(11, new Tile(new LineType[] {
                 LineType.YELLOW, LineType.YELLOW, LineType.NONE, 
                 LineType.NONE, LineType.NONE, LineType.NONE }));
     }
@@ -131,9 +131,9 @@ public class BoardTest {
 
     @Test
     public void testIsEmpty() {
-        assertTrue(emptyBoard.isEmpty() == true);
-        assertTrue(invalidBoard.isEmpty() == false);
-        assertTrue(validBoard.isEmpty() == false);
+        assertTrue(EMPTY_BOARD.isEmpty() == true);
+        assertTrue(INVALID_BOARD.isEmpty() == false);
+        assertTrue(VALID_BOARD.isEmpty() == false);
     }
 
     @Test
@@ -204,8 +204,8 @@ public class BoardTest {
         assertTrue(test.isValid() == true); //Example in code snippet 3
         test.rotateTileClockwise(1);
         assertTrue(test.isValid() == false); //Example in code snippet 3
-        assertTrue(validBoard.isValid() == true);
-        assertTrue(invalidBoard.isValid() == false);
+        assertTrue(VALID_BOARD.isValid() == true);
+        assertTrue(INVALID_BOARD.isValid() == false);
     }
 
     @Test
@@ -231,30 +231,29 @@ public class BoardTest {
         expectedResult = LineType.NONE;
         assertTrue(actualResult == expectedResult);
         
-        assertTrue(emptyBoard.getConnectedPathColor(new int[] {0, 3}) == LineType.NONE);
-        assertTrue(emptyBoard.getConnectedPathColor(new int[] {1, 4}) == LineType.NONE);
-        assertTrue(emptyBoard.getConnectedPathColor(new int[] {1, 2, 4}) == LineType.NONE);
-        assertTrue(emptyBoard.getConnectedPathColor(new int[] {4, 5, 7}) == LineType.NONE);
-        assertTrue(emptyBoard.getConnectedPathColor(new int[] {4, 5, 7, 6, 4, 5, 7}) == LineType.NONE);
-        assertTrue(emptyBoard.getConnectedPathColor(new int[] {8, 10, 11}) == LineType.NONE);
-        assertTrue(emptyBoard.getConnectedPathColor(new int[] {8, 10, 11, 8}) == LineType.NONE);
+        assertTrue(EMPTY_BOARD.getConnectedPathColor(new int[] {0, 3}) == LineType.NONE);
+        assertTrue(EMPTY_BOARD.getConnectedPathColor(new int[] {1, 4}) == LineType.NONE);
+        assertTrue(EMPTY_BOARD.getConnectedPathColor(new int[] {1, 2, 4}) == LineType.NONE);
+        assertTrue(EMPTY_BOARD.getConnectedPathColor(new int[] {4, 5, 7}) == LineType.NONE);
+        assertTrue(EMPTY_BOARD.getConnectedPathColor(new int[] {4, 5, 7, 6, 4, 5, 7}) == LineType.NONE);
+        assertTrue(EMPTY_BOARD.getConnectedPathColor(new int[] {8, 10, 11}) == LineType.NONE);
+        assertTrue(EMPTY_BOARD.getConnectedPathColor(new int[] {8, 10, 11, 8}) == LineType.NONE);
         
-        assertTrue(validBoard.getConnectedPathColor(new int[] {0, 3}) == LineType.NONE);
-        assertTrue(validBoard.getConnectedPathColor(new int[] {1, 4}) == LineType.YELLOW);
-        assertTrue(validBoard.getConnectedPathColor(new int[] {1, 2, 4}) == LineType.NONE);
-        assertTrue(validBoard.getConnectedPathColor(new int[] {4, 5, 7}) == LineType.GREEN);
-        assertTrue(validBoard.getConnectedPathColor(new int[] {4, 5, 7, 6, 4, 5, 7}) == LineType.GREEN);
-        assertTrue(validBoard.getConnectedPathColor(new int[] {8, 10, 11}) == LineType.YELLOW);
-        assertTrue(validBoard.getConnectedPathColor(new int[] {8, 10, 11, 8}) == LineType.YELLOW);
+        assertTrue(VALID_BOARD.getConnectedPathColor(new int[] {0, 3}) == LineType.NONE);
+        assertTrue(VALID_BOARD.getConnectedPathColor(new int[] {1, 4}) == LineType.YELLOW);
+        assertTrue(VALID_BOARD.getConnectedPathColor(new int[] {1, 2, 4}) == LineType.NONE);
+        assertTrue(VALID_BOARD.getConnectedPathColor(new int[] {4, 5, 7}) == LineType.GREEN);
+        assertTrue(VALID_BOARD.getConnectedPathColor(new int[] {4, 5, 7, 6, 4, 5, 7}) == LineType.GREEN);
+        assertTrue(VALID_BOARD.getConnectedPathColor(new int[] {8, 10, 11}) == LineType.YELLOW);
+        assertTrue(VALID_BOARD.getConnectedPathColor(new int[] {8, 10, 11, 8}) == LineType.YELLOW);
         
-        assertTrue(invalidBoard.getConnectedPathColor(new int[] {0, 3}) == LineType.NONE);
-        assertTrue(invalidBoard.getConnectedPathColor(new int[] {1, 4}) == LineType.NONE);
-        assertTrue(invalidBoard.getConnectedPathColor(new int[] {1, 2, 4}) == LineType.NONE);
-        assertTrue(invalidBoard.getConnectedPathColor(new int[] {4, 5, 7}) == LineType.GREEN);
-        assertTrue(invalidBoard.getConnectedPathColor(new int[] {4, 5, 7, 6, 4, 5, 7}) == LineType.NONE);
-        assertTrue(invalidBoard.getConnectedPathColor(new int[] {8, 10, 11}) == LineType.YELLOW);
-        assertTrue(invalidBoard.getConnectedPathColor(new int[] {8, 10, 11, 8}) == LineType.NONE);
-        
+        assertTrue(INVALID_BOARD.getConnectedPathColor(new int[] {0, 3}) == LineType.NONE);
+        assertTrue(INVALID_BOARD.getConnectedPathColor(new int[] {1, 4}) == LineType.NONE);
+        assertTrue(INVALID_BOARD.getConnectedPathColor(new int[] {1, 2, 4}) == LineType.NONE);
+        assertTrue(INVALID_BOARD.getConnectedPathColor(new int[] {4, 5, 7}) == LineType.GREEN);
+        assertTrue(INVALID_BOARD.getConnectedPathColor(new int[] {4, 5, 7, 6, 4, 5, 7}) == LineType.NONE);
+        assertTrue(INVALID_BOARD.getConnectedPathColor(new int[] {8, 10, 11}) == LineType.YELLOW);
+        assertTrue(INVALID_BOARD.getConnectedPathColor(new int[] {8, 10, 11, 8}) == LineType.NONE);
     }
 
     @Test
@@ -267,7 +266,7 @@ public class BoardTest {
         builder.append("------;------;------;\n");
         builder.append("------;------;------;\n");
         builder.append("------;------;------;");
-        actualResult = emptyBoard.toString();
+        actualResult = EMPTY_BOARD.toString();
         expectedResult = builder.toString();
         
         assertThat(actualResult, is(expectedResult));
@@ -277,7 +276,7 @@ public class BoardTest {
         builder.append("------;RGRGYY;GG----;\n");
         builder.append("Y--YGG;G--RGR;-YY---;\n");
         builder.append("------;---YY-;YY----;");
-        actualResult = invalidBoard.toString();
+        actualResult = INVALID_BOARD.toString();
         expectedResult = builder.toString();
         
         assertThat(actualResult, is(expectedResult));
@@ -287,7 +286,7 @@ public class BoardTest {
         builder.append("------;RGRGYY;GG----;\n");
         builder.append("--YGGY;G--RGR;-YY---;\n");
         builder.append("------;---YY-;Y----Y;");
-        actualResult = validBoard.toString();
+        actualResult = VALID_BOARD.toString();
         expectedResult = builder.toString();
         
         assertThat(actualResult, is(expectedResult));
