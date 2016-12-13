d2918ddcc33e9b9f8b1fdd20caeac82e9da6e5a1
Jose Toledo <joset2.91@gmail.com>
2016-12-13 05:20:38 +0100

    Commented not implemented test methods

--- src/test/java/calendar/DateTimeTest.java
+++ src/test/java/calendar/DateTimeTest.java
@@ -16,7 +16,12 @@ public class DateTimeTest {
     
     @Test
     public void testDateTime() {
-        fail("Not yet implemented");
+        Date testDate = new Date(2016, 1, 1);
+        Time testTime = Time.MIN_VAL;
+        DateTime testDateTime = new DateTime(testDate,  testTime);
+        
+        assertTrue(testDateTime != null);
+        assertThat(testDateTime.toString(), is("01-01-2016T00:00:00"));
     }
 
     @Test
@@ -25,10 +30,7 @@ public class DateTimeTest {
         Time testTime = Time.MIN_VAL;
         DateTime testDateTime = new DateTime(testDate,  testTime);
 
-        Date actualResult = testDateTime.getDate();
-        Date expectedResult = testDate;
-
-        assertThat(expectedResult.toString(),  is(actualResult.toString()));
+        assertThat(testDateTime.getDate().toString(),  is(testDate.toString()));
     }
 
     @Test
@@ -37,10 +39,7 @@ public class DateTimeTest {
         Time testTime = Time.MIN_VAL;
         DateTime testDateTime = new DateTime(testDate,  testTime);
 
-        Time actualResult = testDateTime.getTime();
-        Time expectedResult = testTime;
-
-        assertThat(expectedResult.toString(),  is(actualResult.toString()));
+        assertThat(testDateTime.getTime().toString(),  is(testTime.toString()));
     }
 
     @Test
@@ -49,117 +48,111 @@ public class DateTimeTest {
         Time testTime = new Time(5, 3, 4);
         DateTime testDateTime = new DateTime(testDate,  testTime);
 
-        String actualResult = testDateTime.toString();
-        String expectedResult = "01-01-2016T05:03:04";
-
-        assertThat(expectedResult,  is(actualResult));
-
-
+        assertThat(testDateTime.toString(),  is("01-01-2016T05:03:04"));
     }
 
-    @Test
-    public void testGetYear() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetMonthValue() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetMonth() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetDayOfYear() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetDayOfMonth() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetDayOfWeek() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetHour() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetMinute() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testGetSecond() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testPlusDateTime() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testMinusDateTime() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testPlusDate() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testMinusDate() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testPlusTime() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testMinusTime() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testPlusYears() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testPlusMonths() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testPlusDays() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testMinusYears() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testMinusMonths() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testMinusDays() {
-        fail("Not yet implemented");
-    }
-
+//    @Test
+//    public void testGetYear() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetMonthValue() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetMonth() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetDayOfYear() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetDayOfMonth() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetDayOfWeek() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetHour() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetMinute() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testGetSecond() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testPlusDateTime() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testMinusDateTime() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testPlusDate() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testMinusDate() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testPlusTime() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testMinusTime() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testPlusYears() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testPlusMonths() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testPlusDays() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testMinusYears() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testMinusMonths() {
+//        fail("Not yet implemented");
+//    }
+//
+//    @Test
+//    public void testMinusDays() {
+//        fail("Not yet implemented");
+//    }
 }
