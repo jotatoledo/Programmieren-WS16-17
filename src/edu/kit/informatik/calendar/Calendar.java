938dd5b50e9b4cae868ac3467a297531617c6cea
Jose Toledo <joset2.91@gmail.com>
2017-01-07 16:37:03 +0100

    Corrected functionality of get(Appointment)

--- src/edu/kit/informatik/calendar/Calendar.java
+++ src/edu/kit/informatik/calendar/Calendar.java
@@ -1,24 +1,44 @@
 package edu.kit.informatik.calendar;
 
-
 import main.java.sortedList.LinkedSortedAppendList;
 import main.java.sortedList.SortedIterator;
 
+/**
+ * 
+ * @author JoseNote
+ * @version 1.00
+ */
 public class Calendar {
     private LinkedSortedAppendList<Appointment> list;
     
+    /**
+     * Empty constructor
+     */
     public Calendar() {
         list = new LinkedSortedAppendList<Appointment>();
     }
     
+    /**
+     * 
+     * @param element
+     */
     public void add(Appointment element) {
         list.addSorted(element);
     }
     
+    /**
+     * 
+     * @return
+     */
     public SortedIterator<Appointment> get() {
         return list.iterator();
     }
     
+    /**
+     * 
+     * @param pointOfTime
+     * @return
+     */
     public SortedIterator<Appointment> get(DateTime pointOfTime) {
         LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
         SortedIterator<Appointment> it = get();
@@ -31,6 +51,11 @@ public class Calendar {
         return filtered.iterator();
     }
     
+    /**
+     * 
+     * @param date
+     * @return
+     */
     public SortedIterator<Appointment> get(Date date) {
         LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
         SortedIterator<Appointment> it = get();
@@ -46,6 +71,12 @@ public class Calendar {
         return filtered.iterator();
     }
     
+    /**
+     * 
+     * @param start
+     * @param end
+     * @return
+     */
     public SortedIterator<Appointment> get(DateTime start, DateTime end) {
         LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
         SortedIterator<Appointment> it = get();
@@ -59,6 +90,11 @@ public class Calendar {
         return filtered.iterator();
     }
     
+    /**
+     * 
+     * @param name
+     * @return
+     */
     public Appointment get(String name) {
         Appointment current = null;
         SortedIterator<Appointment> it = get();
@@ -70,6 +106,11 @@ public class Calendar {
         return null;
     }
     
+    /**
+     * 
+     * @param appointment
+     * @return
+     */
     public SortedIterator<Appointment> get(Appointment appointment) {
         LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
         SortedIterator<Appointment> it = get();
@@ -77,8 +118,12 @@ public class Calendar {
         
         while (it.hasNext()) {
             current = it.next();
-            if (current.getFrom().compareTo(appointment.getFrom()) >= 0 
-                    || current.getTo().compareTo(appointment.getTo()) <= 0)
+            if (appointment.equals(current)) continue;
+            if ((current.getFrom().compareTo(appointment.getFrom()) >= 0 
+                    && current.getFrom().compareTo(appointment.getTo()) <= 0)
+                    || 
+                    (current.getTo().compareTo(appointment.getFrom()) >= 0 
+                    && current.getTo().compareTo(appointment.getTo()) <= 0))
                 filtered.addSorted(current);
         }
         return filtered.iterator();
