463fd9f83649479e9af959afbb63f7d62f594807
Jose Toledo <joset2.91@gmail.com>
2017-01-07 03:12:32 +0100

    Added commands enum

---
+++ src/main/java/calendar/management/CommandPrefix.java
@@ -0,0 +1,21 @@
+package main.java.calendar.management;
+
+public enum CommandPrefix {
+    QUIT("quit"),
+    ADD_APPOINTMENT("add appointment"),
+    PRINT_APPOINTMENTS("print appointments"),
+    PRINT_APPOINTMENTS_BEFORE("print appointments that start before"),
+    PRINT_APPOINTMENTS_ON("print appointments on"),
+    PRINT_APPOINTMENTS_INTERVAL("print appointments in interval"),
+    PRINT_APPOINTMENTS_CONFLICT("print appointments that conflict with");
+    
+    private final String representation;
+        
+    CommandPrefix(String representation) {
+        this.representation = representation;
+    }
+    
+    public String getRepresentation() {
+        return representation;
+    }
+}
