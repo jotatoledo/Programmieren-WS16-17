853f9a14bfc179d3a13aca3008eb21bd50cef702
Jose Toledo <joset2.91@gmail.com>
2017-01-08 16:26:01 +0100

    Added internal documentation

--- src/main/java/sortedList/SortedIterator.java
+++ src/main/java/sortedList/SortedIterator.java
@@ -1,6 +1,21 @@
 package main.java.sortedList;
 
+/**
+ * Interface of a sorted iterator
+ * @author JoseNote
+ *
+ * @param <T>
+ */
 public interface SortedIterator<T extends Comparable<T>> {
+    /**
+     * Checks if the iterator has a next value
+     * @return {@code True} if the iterator has a next element. {@code False} otherwise.
+     */
     public boolean hasNext();
+    
+    /**
+     * Gets the next value and moves the iterator to the next element
+     * @return The current {@linkplain T} value.
+     */
     public T next();
 }
