53ed316cdaffabcb9f3b51689b3c9e05324179d5
s_toledonavarro <s_toledonavarro@141.3.12.141>
2017-01-09 14:30:26 +0100

    Completed internal doc

--- src/edu/kit/informatik/calendar/Calendar.java
+++ src/edu/kit/informatik/calendar/Calendar.java
@@ -4,12 +4,12 @@ import edu.kit.informatik.list.LinkedSortedAppendList;
 import edu.kit.informatik.list.SortedIterator;
 
 /**
- * 
+ * Offers functionalities to manage a collection of {@linkplain Appointment appointments}
  * @author JoseNote
  * @version 1.00
  */
-public class Calendar {
-    private LinkedSortedAppendList<Appointment> list;
+public final class Calendar {
+    private final LinkedSortedAppendList<Appointment> list;
     
     /**
      * Empty constructor
@@ -19,49 +19,50 @@ public class Calendar {
     }
     
     /**
-     * 
-     * @param element
+     * Adds a new {@linkplain Appointment} instance to the set contained in this
+     * @param element The new instance to be added
      */
-    public void add(Appointment element) {
+    public void add(final Appointment element) {
         list.addSorted(element);
     }
     
     /**
-     * 
-     * @return
+     * Get all the {@linkplain Appointment} instances stored in this
+     * @return 
      */
     public SortedIterator<Appointment> get() {
         return list.iterator();
     }
     
     /**
-     * 
-     * @param pointOfTime
-     * @return
+     * Get the {@linkplain Appointment} instances stored in this that start before a given {@linkplain DateTime} instance
+     * @param pointOfTime The given instance
+     * @return An iterator associated to the set of {@linkplain Appointment} instances which start before the given {@linkplain DateTime} instance
      */
-    public SortedIterator<Appointment> get(DateTime pointOfTime) {
-        LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
-        SortedIterator<Appointment> it = get();
+    public SortedIterator<Appointment> get(final DateTime pointOfTime) {
+    	final LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
+    	final SortedIterator<Appointment> it = get();
         Appointment current = null;
         
         while (it.hasNext()) {
             current = it.next();
-            if (current.getFrom().isBefore(pointOfTime))filtered.addSorted(current);
+            if (current.getFrom().isBefore(pointOfTime))
+            	filtered.addSorted(current);
         }
         return filtered.iterator();
     }
     
     /**
-     * 
-     * @param date
-     * @return
+     * Get the {@linkplain Appointment} instances stored in this which start and end in a given day
+     * @param date The day to match
+     * @return An iterator associated to the set of {@linkplain Appointment} instances which start and end in the given day
      */
-    public SortedIterator<Appointment> get(Date date) {
-        LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
-        SortedIterator<Appointment> it = get();
+    public SortedIterator<Appointment> get(final Date date) {
+    	final LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
+        final SortedIterator<Appointment> it = get();
         Appointment current = null;
-        DateTime min = date.atTime(new Time(0, 0, 0));
-        DateTime max = date.atTime(new Time(23, 59, 59));
+        final DateTime min = date.atTime(new Time(0, 0, 0));
+        final DateTime max = date.atTime(new Time(23, 59, 59));
         
         while (it.hasNext()) {
             current = it.next();
@@ -72,14 +73,14 @@ public class Calendar {
     }
     
     /**
-     * 
-     * @param start
-     * @param end
-     * @return
+     * Get the {@linkplain Appointment} instances stored in this which are contained in a given time interval
+     * @param start The start of the time interval
+     * @param end The end of the time interval
+     * @return An iterator associated to the set of {@linkplain Appointment} instances contained in the time interval
      */
-    public SortedIterator<Appointment> get(DateTime start, DateTime end) {
-        LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
-        SortedIterator<Appointment> it = get();
+    public SortedIterator<Appointment> get(final DateTime start, final DateTime end) {
+    	final LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
+        final SortedIterator<Appointment> it = get();
         Appointment current = null;
         
         while (it.hasNext()) {
@@ -91,34 +92,38 @@ public class Calendar {
     }
     
     /**
-     * 
-     * @param name
-     * @return
+     * Searches in the stored {@linkplain Appointment} instances in this for one with a specific name.
+     * @param name The name to match
+     * @return A {@linkplain Appointment} instance with the given name. {@value Null} if such instance isn't stored in this
      */
-    public Appointment get(String name) {
-        Appointment current = null;
-        SortedIterator<Appointment> it = get();
+    public Appointment get(final String name) {
+    	final SortedIterator<Appointment> it = get();
+    	Appointment current = null;    	
         
         while (it.hasNext()) {
             current = it.next();
-            if (current.getName().equals(name)) return current;
+            if (current.getName().equals(name))
+            	return current;
         }
         return null;
     }
     
     /**
-     * 
-     * @param appointment
-     * @return
+     * Get the {@linkplain Appointment} instances stored in this that conflict with a given {@linkplain Appointment} instance, which is stored in this too. 
+     * @param appointment The instance to match against
+     * @return An iterator associated to the set of {@linkplain Appointment} instances which conflict with the given one
      */
-    public SortedIterator<Appointment> get(Appointment appointment) {
-        LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
-        SortedIterator<Appointment> it = get();
+    public SortedIterator<Appointment> get(final Appointment appointment) {
+    	final LinkedSortedAppendList<Appointment> filtered = new LinkedSortedAppendList<Appointment>();
+        final SortedIterator<Appointment> it = get();
         Appointment current = null;
         
         while (it.hasNext()) {
             current = it.next();
-            if (appointment.equals(current)) continue;
+            if (appointment.equals(current))
+            	//true: the given instance is the same as the actual instance of the loop.
+            	//An instance doesn't conflict with itself. Just skip
+            	continue;
             if ((current.getFrom().compareTo(appointment.getFrom()) >= 0 
                     && current.getFrom().compareTo(appointment.getTo()) <= 0)
                     || 
