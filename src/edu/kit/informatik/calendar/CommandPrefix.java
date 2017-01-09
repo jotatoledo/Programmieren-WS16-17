070d51ce733aa467f3b99e1086ff11fafb80ff85
Jose Toledo <joset2.91@gmail.com>
2017-01-09 11:57:51 +0100

    Added internal doc

--- src/edu/kit/informatik/calendar/CommandPrefix.java
+++ src/edu/kit/informatik/calendar/CommandPrefix.java
@@ -1,12 +1,33 @@
 package edu.kit.informatik.calendar;
 
 public enum CommandPrefix {
+    /**
+     * Prefix for the quit command
+     */
     QUIT("quit"),
+    /**
+     * Prefix for the add command
+     */
     ADD_APPOINTMENT("add appointment"),
+    /**
+     * Prefix for the simple print command
+     */
     PRINT_APPOINTMENTS("print appointments"),
+    /**
+     * Prefix for the print command of appointments that start before a given date
+     */
     PRINT_APPOINTMENTS_BEFORE("print appointments that start before"),
+    /**
+     * Prefix for the print command of appointments on a given day
+     */
     PRINT_APPOINTMENTS_ON("print appointments on"),
+    /**
+     * Prefix for the print command of appointments contained in a given time interval
+     */
     PRINT_APPOINTMENTS_INTERVAL("print appointments in interval"),
+    /**
+     * Prefix for the print command of appointments that conflict with a given one
+     */
     PRINT_APPOINTMENTS_CONFLICT("print appointments that conflict with");
     
     private final String representation;
