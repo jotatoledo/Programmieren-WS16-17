4dccfdf96728ea58f0167c628e4d62aa6e28c941
Jose Toledo <joset2.91@gmail.com>
2017-02-12 23:50:37 +0100

    Added keyword class

--- src/edu/kit/informatik/literatur_system/Keyword.java
+++ src/edu/kit/informatik/literatur_system/Keyword.java
@@ -1,12 +1,16 @@
 package edu.kit.informatik.literatur_system;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * TODO add doc
  * @author JoseNote
  * @version %I%, %G%
  */
-public final class Keyword {
+public final class Keyword implements Comparable<Keyword> {
     private final String word;
+    private final Set<ITaged> tagedElements;
     
     /**
      * TODO add doc
@@ -14,5 +18,23 @@ public final class Keyword {
      */
     public Keyword(final String word) {
         this.word = word;
+        //TODO edit set
+        tagedElements = new HashSet<ITaged>();
+   }
+    
+    @Override
+    public int compareTo(final Keyword o) {
+        return word.compareTo(o.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return word.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Keyword
+                && compareTo((Keyword) obj) == 0;
     }
 }
