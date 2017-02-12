836fe5863ff25353bcb6200f4e1c6f7f2745fe50
Jose Toledo <joset2.91@gmail.com>
2017-02-12 01:22:26 +0100

    Implemented add/exist author

--- src/edu/kit/informatik/literatur_system/ILiteraturSystemService.java
+++ src/edu/kit/informatik/literatur_system/ILiteraturSystemService.java
@@ -6,5 +6,19 @@ package edu.kit.informatik.literatur_system;
  * @version %I%, %G%
  */
 public interface ILiteraturSystemService {
-
+    /**
+     * Creates a new author entity and persists it in the service
+     * @param firstName the first name value for the new entity
+     * @param lastName the last name value for the new entity
+     * @return the new entity
+     */
+    Author addAuthor(String firstName, String lastName);
+    
+    /**
+     * Checks for the existence of an author
+     * @param firstName the first name value to match against
+     * @param lastName the last name value to match against
+     * @return {@code True} if there is an author with the given parameters. {@code False} otherwise
+     */
+    boolean existAuthor(String firstName, String lastName);
 }
--- src/edu/kit/informatik/literatur_system/LiteraturSystemService.java
+++ src/edu/kit/informatik/literatur_system/LiteraturSystemService.java
@@ -1,5 +1,53 @@
 package edu.kit.informatik.literatur_system;
 
-public class LiteraturSystemService implements ILiteraturSystemService {
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * Implementation of the literature system service
+ * @author JoseNote
+ * @version %I%, %G%
+ */
+public class LiteraturSystemService implements ILiteraturSystemService {
+    private final Set<Author> authors;
+    
+    /**
+     * Creates a new instance of the service.
+     * Note: good point to use dependency injection
+     */
+    public LiteraturSystemService() {
+        authors = new TreeSet<Author>();
+    }
+
+    @Override
+    public Author addAuthor(final String firstName, final String lastName) {
+        //TODO validate fields
+        if (existAuthor(firstName, lastName))
+            throw new IllegalArgumentException("author already exist");
+        Author entity = new Author(firstName, lastName);
+        if (!authors.add(entity))
+            throw new IllegalArgumentException("error by addition");
+        return entity;
+    }
+
+    @Override
+    public boolean existAuthor(final String firstName, final String lastName) {
+        return authors.stream()
+                .filter(x -> x.getLastName().compareTo(lastName) == 0)
+                .filter(x -> x.getFirstName().compareTo(firstName) == 0)
+                .findFirst()
+                .isPresent();
+                
+    }
+    
+    private IllegalArgumentException noSuch(
+            final Class<?> type,
+            final Object arg,
+            final Object... args) {
+        ////
+        return new IllegalArgumentException(Stream.concat(Stream.of(arg), Stream.of(args)).map(String::valueOf)
+                .collect(Collectors.joining(", ", "No such " + type.getSimpleName() + ": ", "")));
+    }
 }
