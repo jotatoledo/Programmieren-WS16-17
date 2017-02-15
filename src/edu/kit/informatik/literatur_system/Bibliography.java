33dae9062d3af060cd552a37d43e33e0fc1424e1
Jose Toledo <joset2.91@gmail.com>
2017-02-15 15:03:37 +0100

    Added bibliography structures

---
+++ src/edu/kit/informatik/literatur_system/Bibliography.java
@@ -0,0 +1,25 @@
+package edu.kit.informatik.literatur_system;
+
+import java.util.Collection;
+
+public abstract class Bibliography {
+    private final short publicationYear;
+    private final String articleTitle;
+    private final Collection<AuthorNames> authors;
+    
+    /**
+     * TODO add doc
+     * @param authors TODO add doc
+     * @param publicationYear TODO add doc
+     * @param articleTitle TODO add doc
+     */
+    protected Bibliography(
+            final Collection<AuthorNames> authors, final short publicationYear, final String articleTitle) {
+        this.publicationYear = publicationYear;
+        this.articleTitle = articleTitle;
+        this.authors = authors;
+    }
+    
+    @Override
+    public abstract String toString();
+}
---
+++ src/edu/kit/informatik/literatur_system/ChicagoFormat.java
@@ -0,0 +1,12 @@
+package edu.kit.informatik.literatur_system;
+
+import java.util.Collection;
+
+public abstract class ChicagoFormat extends Bibliography {
+
+    protected ChicagoFormat(
+            final Collection<AuthorNames> authors, final short publicationYear, final String articleTitle) {
+        super(authors, publicationYear, articleTitle);
+        // TODO Auto-generated constructor stub
+    }
+}
