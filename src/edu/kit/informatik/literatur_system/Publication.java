d0cbccab399490b876836f8c87789c5154e290fa
Jose Toledo <joset2.91@gmail.com>
2017-03-08 13:11:24 +0100

    Removed: unused import

--- src/edu/kit/informatik/literatur_system/Publication.java
+++ src/edu/kit/informatik/literatur_system/Publication.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import edu.kit.informatik.InvalidRelationException;
 import edu.kit.informatik.Utilities;
 
 /**
